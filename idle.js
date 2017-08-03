a5b10d2e1b3ae93407ed7ead0f3009494bef153d
zoviq <lazaruyk16@mail.ru>
2017-08-03 20:57:14 +0300

    Update idle.js

--- idle.js
+++ idle.js
@@ -1,9 +1,6 @@
 var http = require('http');
-var static = require('node-static');
-var file = new static.Server('.');
 
-http.createServer(function(req, res) {
-  file.serve(req, res);
-}).listen(8080);
-
-console.log('Server running on port 8080');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(8080); 
