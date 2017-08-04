0435415ac4c571343d29fbf58428dbb81b3de0e6
zoviq <lazaruyk16@mail.ru>
2017-08-04 15:37:36 +0300

    Update idle.js

--- idle.js
+++ idle.js
@@ -1,5 +1,8 @@
 var http = require('http');
+var path = require('path');
 
+  app.use(express.static(path.join(__dirname, 'public')));
+    
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World!');
