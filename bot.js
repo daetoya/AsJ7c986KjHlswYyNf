dbd9bb5bde23cdbcf2ba189ae7c8262d0429018e
daetoya <lazaruyk16@mail.ru>
2018-02-24 14:50:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,9 @@
 const SteamUser = require('steam-user');
 const client = new SteamUser();
-const config = require('./config.json');
 
 const logOnOptions = {
-  accountName: config.username,
-  password: config.password
+  accountName: zloiskif,
+  password: lazaruyk16
 };
 
 client.logOn(logOnOptions);
