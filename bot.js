bccc239e80a68665a5a1405b3003cee7cb798abd
daetoya <lazaruyk16@mail.ru>
2018-02-24 14:29:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,17 @@
-s
+const SteamUser = require('steam-user');
+const client = new SteamUser();
+const config = require('./config.json');
+
+const logOnOptions = {
+  accountName: config.username,
+  password: config.password
+};
+
+client.logOn(logOnOptions);
+
+client.on('loggedOn', () => {
+  console.log('Logged into Steam!');
+
+  client.setPersona(SteamUser.Steam.EPersonaState.Online);
+  client.gamesPlayed(33930,107410,224580);
+});
