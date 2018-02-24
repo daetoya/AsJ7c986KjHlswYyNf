20e6c4d3d3ed80a9dfd8dfdf4973090c0fc25dfe
daetoya <lazaruyk16@mail.ru>
2018-02-24 15:00:47 +0300

    Create 133.js

---
+++ 133.js
@@ -0,0 +1,16 @@
+const SteamUser = require('steam-user');
+const client = new SteamUser();
+
+const logOnOptions = {
+  accountName: 'zloiskif',
+  password: 'lazaruyk16'
+};
+
+client.logOn(logOnOptions);
+
+client.on('loggedOn', () => {
+  console.log('Logged into Steam!');
+
+  client.setPersona(SteamUser.Steam.EPersonaState.Online);
+  client.gamesPlayed([33930,107410,224580]);
+});
