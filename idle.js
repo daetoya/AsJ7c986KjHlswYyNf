f3c9548aa291a91b2e23a7a328ffc45e9ef5a4f7
daetoya <lazaruyk16@mail.ru>
2018-02-24 15:30:44 +0300

    Update idle.js

--- idle.js
+++ idle.js
@@ -1,1 +1,17 @@
-1
+const SteamUser = require('steam-user');
+const client = new SteamUser();
+const SteamTotp = require('steam-totp');
+
+const logOnOptions = {
+  accountName: 'zloiskif',
+  password: 'lazaruyk16'
+};
+
+client.logOn(logOnOptions);
+
+client.on('loggedOn', () => {
+  console.log('Logged into Steam!');
+
+  client.setPersona(SteamUser.Steam.EPersonaState.Online);
+  client.gamesPlayed([33930,107410,224580]);
+});
