e36bd41f7c304e4baf66382d55bfbf70e6806ea4
daetoya <lazaruyk16@mail.ru>
2018-02-28 16:20:11 +0300

    Update neeconor1.js

--- neeconor1.js
+++ neeconor1.js
@@ -1,1 +1,22 @@
 
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = "!N";
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix)) return;	
+	
+  if (message.content.startsWith(prefix + "Test")) {
+    message.channel.send("Active");
+  } else
+
+  if (message.content.startsWith(prefix + "Тест")) {
+    message.channel.send("Active");
+  }
+});
+
+client.login("NDE1NTg2MDU0MDAzODg0MDMz.DXMf4Q.Acea5QE-72NpNbIVaNV0cvqVSGY");
