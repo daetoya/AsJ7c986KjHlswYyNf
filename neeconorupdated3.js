3ae32dfa64f19d531a3f7f701c3f83032bcdf56e
daetoya <lazaruyk16@mail.ru>
2018-02-28 17:58:21 +0300

    Update and rename neeconorupdated2.js to neeconorupdated3.js

--- neeconorupdated3.js
+++ neeconorupdated3.js
@@ -8,7 +8,7 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;	
