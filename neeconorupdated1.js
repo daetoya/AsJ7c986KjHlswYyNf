080c34890fdbcf159192432529483537ee0b4b71
daetoya <lazaruyk16@mail.ru>
2018-02-28 17:52:42 +0300

    Update and rename neeconorupdated.js to neeconorupdated1.js

--- neeconorupdated1.js
+++ neeconorupdated1.js
@@ -1,11 +1,12 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require ("./config.json");
-const args = message.content.slice(prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
+
 
 client.on("ready", () => {
   console.log("I am ready!");
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
 });
 
 client.on("message", (message) => {
