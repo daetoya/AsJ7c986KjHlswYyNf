54d04410ff13b889d55fe51d6d8025bc72831609
daetoya <lazaruyk16@mail.ru>
2018-02-28 17:26:45 +0300

    Update and rename neeconor3.js to neeconor4.js

---
+++ neeconor4.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require ("./config.json");
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;	
+	
+  if (message.content.startsWith(config.prefix + "help")) {
+    message.channel.send("Список команд: help, playmusic, pausemusic, resumemusic, stopmusic, wotstats, lvl");
+  } else
+
+  if (message.content.startsWith(config.prefix + "Тест")) {
+    message.channel.send("Active");
+  }
+});
+
+client.on("roleCreate", (roleCreate) => {
+	message.channel.send("A new role was created!");
+});
+client.login(config.token);
